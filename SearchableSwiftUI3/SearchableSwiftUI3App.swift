6b7ea166aac8b93b4e64ad512a49f27fe182b2cc
Ionut Dirlea <ionut.dirlea@gmail.com>
2021-07-28 07:45:52 +0300

    Initial Commit

---
+++ SearchableSwiftUI3/SearchableSwiftUI3App.swift
@@ -0,0 +1,17 @@
+//
+//  SearchableSwiftUI3App.swift
+//  SearchableSwiftUI3
+//
+//  Created by Ionut Dirlea on 28.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct SearchableSwiftUI3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
